6016dd816fbe2077773e7f88d3835eb872c55203
Khurram Shahzad <macpro@macpros-MBP-2.home>
2019-01-19 15:15:46 +0000

    Override ToString method for Movie class

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -5,7 +5,11 @@ namespace Models
     public class Movie
     {
         public string Name { get; set; }
-        public DateTime RelaseYear { get; set; }
+        public DateTime ReleaseYear { get; set; }
         public string Country { get; set; }
+
+        public override string  ToString() {
+            return $"Name:{Name},ReleaseYear:{ReleaseYear.ToString()},Country:{Country}";
+        }
     }
 }
